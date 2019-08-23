9301513c793c7d7f29f1a71c4dde93060861ff5a
M4T <mateo.lesiuk@gmail.com>
2019-08-23 21:57:58 +0200

    Section 4 done

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Items from '../components/Items';
+
 const Home = props => (
   <div>
-    <p>Hey!</p>
+    <Items page={parseFloat(props.query.page) || 1} />
   </div>
 );
 
